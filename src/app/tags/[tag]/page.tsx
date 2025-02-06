afe03b9513a60dce9d310ea49554ecc374588ccd
Chi Wai Chan <chanchiwairay@gmail.com>
2025-02-06 16:25:22 +0800

    Workaround to make /tags/* and /blog has the same layout.

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -51,12 +51,21 @@ export default async function TagPage(props: { params: Promise<{ tag: string }> 
   }
 
   return (
-    <ListLayoutWithTags
-      posts={filteredPosts}
-      initialDisplayPosts={initialDisplayPosts}
-      pagination={pagination}
-      title={title}
-      siteMetadata={siteMetadata}
-    />
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Blog
+          </h1>
+        </div>
+        <ListLayoutWithTags
+          posts={filteredPosts}
+          initialDisplayPosts={initialDisplayPosts}
+          pagination={pagination}
+          title={title}
+          siteMetadata={siteMetadata}
+        />
+      </div>
+    </>
   )
 }
