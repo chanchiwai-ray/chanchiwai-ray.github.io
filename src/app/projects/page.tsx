d0067cfbc0e3d0b2e4898df2fcb68c38f87e1787
Chi Wai Chan <chanchiwairay@gmail.com>
2025-02-17 21:38:36 +0800

    Make the project card can be filtered.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,27 +1,9 @@
 import { genPageMetadata } from "@/app/seo"
 import projects from "@/data/projects"
-import Card from "@/ui/components/Card"
 import ProjectLayout from "@/ui/layouts/ProjectLayout"
 
 export const metadata = genPageMetadata({ title: "Projects" })
 
 export default function Projects() {
-  return (
-    <ProjectLayout>
-      <div className="container py-12">
-        <div className="-m-4 flex flex-wrap">
-          {projects.map((d) => (
-            <Card
-              key={d.name}
-              title={d.name}
-              description={d.description}
-              imgSrc={d.image}
-              href={d.link}
-              technologies={d.technologies}
-            />
-          ))}
-        </div>
-      </div>
-    </ProjectLayout>
-  )
+  return <ProjectLayout projects={projects} />
 }
--- src/ui/components/Card.tsx
+++ src/ui/components/Card.tsx
@@ -1,19 +1,19 @@
 import Image from "./Image"
 import Link from "./Link"
 
-const Card = ({ title, description, imgSrc, href, technologies }) => (
-  <div className="md max-w-[544px] p-4 md:w-1/2">
+const Card = ({ title, description, image, link, technologies, setTech }) => {
+  return (
     <div
       className={`${
-        imgSrc && "h-full"
+        image && "h-full"
       }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
     >
-      {imgSrc &&
-        (href ? (
-          <Link href={href} aria-label={`Link to ${title}`}>
+      {image &&
+        (link ? (
+          <Link href={link} aria-label={`Link to ${title}`}>
             <Image
               alt={title}
-              src={imgSrc}
+              src={image}
               className="object-cover object-center md:h-36 lg:h-48"
               width={544}
               height={306}
@@ -22,7 +22,7 @@ const Card = ({ title, description, imgSrc, href, technologies }) => (
         ) : (
           <Image
             alt={title}
-            src={imgSrc}
+            src={image}
             className="object-cover object-center md:h-36 lg:h-48"
             width={544}
             height={306}
@@ -30,8 +30,8 @@ const Card = ({ title, description, imgSrc, href, technologies }) => (
         ))}
       <div className="p-6">
         <h2 className="text-2xl font-bold leading-8 tracking-tight">
-          {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
+          {link ? (
+            <Link href={link} aria-label={`Link to ${title}`}>
               {title}
             </Link>
           ) : (
@@ -40,15 +40,17 @@ const Card = ({ title, description, imgSrc, href, technologies }) => (
         </h2>
         <div className="flex flex-wrap">
           {technologies?.map((tech: string) => (
-            <p key={tech} className="mr-3 text-sm font-medium uppercase text-primary-500">
-              {tech.split(" ").join("-")}
-            </p>
+            <button key={tech} onClick={() => setTech(tech)}>
+              <p className="mr-3 pt-1 text-sm font-medium uppercase text-primary-500">
+                {tech.split(" ").join("-")}
+              </p>
+            </button>
           ))}
         </div>
-        <div className="prose my-3 max-w-none text-gray-500 dark:text-gray-400">{description}</div>
-        {href && (
+        <div className="prose my-3 max-w-none text-gray-500 hover:text-gray-400">{description}</div>
+        {link && (
           <Link
-            href={href}
+            href={link}
             className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label={`Link to ${title}`}
           >
@@ -57,7 +59,7 @@ const Card = ({ title, description, imgSrc, href, technologies }) => (
         )}
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Card
